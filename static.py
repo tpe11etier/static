13e681c7be71a8098c570522ef15c1ee7fb1e832
Tony Pelletier <tony.pelletier@gmail.com>
2010-10-10 01:08:21 -0400

    First commit - Working code.

---
+++ static.py
@@ -0,0 +1,158 @@
+#!/usr/bin/env python
+
+import sys
+import csv
+import xlrd
+import os
+import shutil
+import commands
+import re
+import smtplib
+import socket
+import time
+import urllib, urllib2, cookielib, MultipartPostHandler, httplib
+from BeautifulSoup import BeautifulSoup
+
+
+
+def main():
+    if len(sys.argv) <= 2:
+        print 'You must pass in two filenames.  First one is the Prompt Sheet xls, second one is the output zip file.'
+        print 'eg. ./dynamic.py voice_prompts.xls voice_prompts.zip'
+        sys.exit(1)
+    else:
+        listdata = readfile(sys.argv[1])
+        zipfile =  sys.argv[2]
+        controlfile()
+        filestozip = getfiles()
+        zip(filestozip, zipfile)
+        opener = authenticate()
+        upload(opener, zipfile)
+
+def readfile(filename):
+    '''Reads xls file passed in and writes out to prompts.csv.'''
+    writer = csv.writer(open('prompts.csv', 'wb'))
+    book = xlrd.open_workbook(filename) 
+    sh = book.sheet_by_index(0) 
+    for rx in range(sh.nrows):
+        writer.writerow((sh.row_values(rx)))
+        
+def clientlist():
+    soup = BeautifulSoup(result)
+    option_dict = {}
+    table = soup.find("select", {"name" : "ahClientList"})
+    for row in table.findChildren('option')[1:]:
+        option_dict[row.get('value')] = row
+    for k, v in option_dict.items():
+        options = '%s, %s' % (k, v.contents)
+    return options
+    
+      
+def controlfile():
+    '''Writes out a text file that handle how the prompts should be processed and with which voice talent.
+       I only have Dynamic prompts built in at this time.'''
+       
+    talents = {'Leslie' : 'F_ENG_4\n',
+                'Maru' : 'F_ESP_4\n',
+               'Dave' : 'M_ENG_4\n',
+               'Fabio' : 'M_ESP_3\n',
+}
+    
+    print "Voice Talents"
+    print "============================================="
+    for k, v in talents.items():
+        print(k)
+    try:
+        talent = raw_input('Please select a voice talent: ')
+        file = open('control.txt', 'wb')
+        file.write('vocab = ' + talents[talent])
+        file.write('plural = N\n')
+        file.write('type = STATIC\n')
+    except (KeyError):
+        print "Voice Talent does not exist.\n"
+        main()
+    except (KeyboardInterrupt):
+        print "Keyboard Interrupt. Exiting."
+        sys.exit(1)
+        
+
+def getfiles():
+    '''Gets list of pcm files'''
+    result = []
+    paths = os.listdir(os.curdir)  # list of paths in that dir
+    for fname in paths:
+        match = re.search(r'.pcm', fname)
+        if match:
+            result.append(os.path.abspath(os.path.join(os.curdir, fname)))
+    return result
+
+    
+def zip(paths, zipfile):
+  """Zip up all of the given files into a new zip file with the given name."""
+  cmd = 'zip -j ' + zipfile + ' ' + 'prompts.csv' + ' ' +  ' ' + 'control.txt' + ' ' + ' '.join(paths) 
+  print "Command I'm going to do: " + cmd 
+  (status, output) = commands.getstatusoutput(cmd)
+  # If command had a problem (status is non-zero),
+  # print its output to stderr and exit.
+  if status:
+    sys.stderr.write(output)
+    sys.exit(1)
+    
+def authenticate():
+    url = 'https://tools.varolii.com/'
+    username = raw_input('Please enter Username: ')
+    password = raw_input('Please enter Password: ')
+    cj = cookielib.CookieJar()
+    ck = cookielib.Cookie(version=0, name='killmenothing', value='', port=None, port_specified=False, domain='tools.varolii.com', domain_specified=False, domain_initial_dot=False, path='/', path_specified=True, secure=False, expires=None, discard=True, comment=None, comment_url=None, rest={'HttpOnly': None}, rfc2109=False)
+    cj.set_cookie(ck)
+    
+    opener = urllib2.build_opener(MultipartPostHandler.MultipartPostHandler, urllib2.HTTPCookieProcessor(cj))
+    login_data = urllib.urlencode({'frmUserID' : username, 'frmPassword' : password, 'formAction' : 'login'})
+    opener.open(url + 'admin_login_page.jsp')
+    resp = opener.open('https://tools.varolii.com/AdminLoginServlet', login_data)
+    result = resp.read()
+    match = (re.search(r'escape\(".*"\)', result))
+    if match is not None:
+        validmatch = match.group()
+    else:
+        print "Login Failed. Exiting."
+        sys.exit(1)
+    sessiontoken = validmatch[8:40]
+    
+    resp = opener.open('https://tools.varolii.com/change_role_db.jsp?roleNum=2&GlobalSessionToken=' + sessiontoken)
+    
+    result = resp.read()
+    
+    soup = BeautifulSoup(result)
+    clientlist_dict = {}
+    table = soup.find("select", {"name" : "ahClientList"})
+    for row in table.findChildren('option')[1:]:
+        clientlist_dict[row.get('value')] = row
+    for k, v in clientlist_dict.items():
+        clients = '%s, %s' % (k, v.contents)
+        print clients
+    selectclient = raw_input('Please enter a client number: ')
+    client = selectclient
+    if client not in clientlist_dict.keys():
+        print 'You must enter a client ID. Exiting.'
+        sys.exit(1)
+    else:
+        resp = opener.open('https://tools.varolii.com/change_client_db.jsp?clientNum=' + client + '&GlobalSessionToken=' + sessiontoken)
+    return opener
+   
+def upload(opener, zipfile):
+    uploadparams = {'uploadFileName':open(zipfile, 'rb')}
+    urllib2.install_opener(opener)
+    req = urllib2.Request('https://tools.varolii.com/VoicePromptBatchUploadServlet', uploadparams)
+    response = urllib2.urlopen(req).read().strip()
+    results = urllib2.urlopen('https://tools.varolii.com/voice_batch_upload_viewer.jsp')
+    uploadresult = results.read()
+    print uploadresult
+    time.sleep(20)
+    results = urllib2.urlopen('https://tools.varolii.com/voice_batch_upload_viewer.jsp')
+    uploadresult = results.read()
+    print uploadresult
+
+      
+if __name__ == '__main__':
+    main()
